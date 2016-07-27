95f83aca4d2805e4e2606565379e1c11e5308636
liangyanqing <liangyanqing@hoolai.com>
2016-07-27 15:57:39 +0800

    feature/login

--- src/components/views/login/index.js
+++ src/components/views/login/index.js
@@ -2,46 +2,56 @@ require('./style.scss');
 module.exports = {
   template: require('./template.html'),
   replace: true,
-  data: function () {
+  data: function() {
     return {
       account: '',
       password: ''
     };
   },
   components: {
+    // require 的是alert下的index.js
     alert: require('../../common/alerts')
   },
   created: function () {
     this.$dispatch('toggleNoauth');
   },
   methods: {
-    login: function () {
+    login: function() {
       var url = '/oauth/token';
       var param = {};
       param.username = this.account;
       param.password = this.password;
       var _this = this;
-      ac_http.oauth(_this, url, param, function (ret) {
+      var cc_http;
+      debugger
+      // module.exports.oauth function ?
+      ac_http.oauth(_this, url, param, function(ret) {
         if (ret.ret < 0) {
-          _this.$broadcast('showAlert', '账号或密码错误！', 3);
+          // 如何实现的广播
+          _this.$broadcast('showAlert', '账号或密码错误!', 2);
           return;
         }
         var token = ret.data;
         var expiredAt = new Date();
+        debugger
         expiredAt.setSeconds(expiredAt.getSeconds() + token.expires_in);
         token.expires_at = expiredAt.getTime();
         ac_store.setToken(token);
+        // 派发事件，沿着父链冒泡 推出去当前消息
         _this.$dispatch('showAlert', '成功登录！');
         _this.getUserInfo();
       });
     },
-    getUserInfo: function () {
+    getUserInfo: function() {
       var url = '/api/account';
       var _this = this;
-      ac_http.request(_this, 'GET', url, function (ret) {
+      ac_http.request(_this, 'GET', url, function(ret) {
         ac_store.setUserInfo(ret.data);
+        // router.go(path)  path跳转信息
         _this.$router.go('/home');
       });
     }
   }
+  
+
 };
