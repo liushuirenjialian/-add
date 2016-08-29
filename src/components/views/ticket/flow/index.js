af732381cbd51e6301f961e2603e53784abde5a4
hoolai <liangyanqing1@hoolai.com>
2016-08-29 15:23:45 +0800

    流转弹框的选择框，自动提示功能

--- src/components/views/ticket/flow/index.js
+++ src/components/views/ticket/flow/index.js
@@ -7,11 +7,25 @@ module.exports = {
     return {
       ticketId: '',
       toUserName: '',
-      fromContent: ''
+      fromContent: '',
+      roleOptions: [],
+      personOptions: [],
+      rolevalue: '',
+      person: '',
+      role: []
     };
+  },
+  components: {
+    vselect: require('../../../common/vSelect')
+  },
+  watch: {
+    rolevalue: function () {
+      this.getPerson();
+    }
   },
   created: function () {
     // this.isshow = true;
+    this.getRole();
   },
   events: {
     loadTicketId: function (ticketId) {
@@ -19,6 +33,40 @@ module.exports = {
     }
   },
   methods: {
+    getRole: function () {
+      var url = '/api/author';
+      var method = 'GET';
+      var _this = this;
+      ac_http.request(_this, method, url, function (res) {
+        if (res.ret < 0) {
+          _this.dispatch('showMsg', res.data.message, 1);
+          return;
+        }
+        _this.rolevalue = res.data[0].text;
+        res.data.forEach(function (item) {
+          _this.roleOptions.push(item.text);
+          _this.role[item.text] = item.name;
+        });
+      });
+    },
+    getPerson: function () {
+      var _this = this;
+      var url = '/api/users/search/' + _this.role[_this.rolevalue];
+      var method = 'GET';
+      ac_http.request(_this, method, url, function (res) {
+        if (res.ret < 0) {
+          _this.dispatch('showMsg', res.data.message, 1);
+          return;
+        }
+        _this.person = '';
+        _this.personOptions = [];
+        _this.person = res.data[0].login;
+        _this.toUserName = res.data[0].login;
+        res.data.forEach(function (item) {
+          _this.personOptions.push(item.login);
+        });
+      });
+    },
     close: function () {
       this.isshow = false;
     },
